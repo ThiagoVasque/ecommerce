a3e81a7a5841c24fe6a8b53fdd3daaab185184e8
thiago <thiagonunesvasque078@gmail.com>
2023-07-12 09:49:11 -0300

    Inserindo PageAdmin e Tpl Admin

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -14,15 +14,15 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array())
+	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
 		    "base_url"      => null,
-		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/ecommerce-hcode/views/",
-		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/ecommerce-hcode/views-cache/",
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 		    "debug"         => false
 		);
 
